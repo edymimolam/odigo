9ae3a272fc0cfe6f0a1ec26f47bac1dcd89135ef
edymimolam <edymimolam@gmail.com>
2019-10-09 02:16:59 +0300

    adaptive layout complete & hover animations added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ var hamburger = document.querySelector(".hamburger");
   var nav = document.querySelector(".main-nav");
   // On click
   hamburger.addEventListener("click", function() {
-    console.log(nav);
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     nav.classList.toggle("is-active");
